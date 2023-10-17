9a2ce90ca28e46466da47c35cf352b1f8bc04e13
AlbertoLutz <albertolutzdias@gmail.com>
2023-10-17 08:14:05 -0300

    Estruturando as rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.modules';
 import { AppComponent } from './app.component';
+
+import { IndexModule } from './pages/index/index.module';
+import { PortfolioModule } from './pages/portfolio/portfolio.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    IndexModule,
+    PortfolioModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
